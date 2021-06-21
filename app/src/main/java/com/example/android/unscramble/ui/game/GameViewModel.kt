22f7247c1005153829d81b6b232f4714d3b95362
Crackmindedsk <shakumbhar@gmail.com>
2021-06-21 23:56:01 +0530

    Viewmodel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
+
 class GameViewModel:ViewModel() {
+    init {
+        Log.d("Game Fragment","GameViewModel created!")
+    }
     private var score=0
     private var currentWordCount=0
     private var _currentScrambledWord="test"
@@ -9,4 +14,14 @@ class GameViewModel:ViewModel() {
     private var _count=0
     val count:Int
     get() = _count
+
+    override fun onCleared(){
+        super.onCleared()
+        Log.d("Gameragment","GameViewModel destroyed!")
+    }
+    override fun onDetach(){
+        super.onDetach()
+        Log.d("GameFragment", "GameFragment destroyed!")
+    }
+
 }
