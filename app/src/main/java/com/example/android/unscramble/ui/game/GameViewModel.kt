35581c6b9677ab8525d6020ed59f430333230258
Crackmindedsk <shakumbhar@gmail.com>
2021-06-22 23:00:57 +0530

    Restart game

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -9,7 +9,10 @@ class GameViewModel: ViewModel() {
         getNextWord()
     }
 
-    private var currentWordCount=0
+    private var _currentWordCount=0
+    val currentWOrdCount:Int
+    get() = _currentWordCount
+
     private lateinit var _currentScrambledWord:String
     val currentScrambledWord:String
     get() = _currentScrambledWord
@@ -39,14 +42,14 @@ class GameViewModel: ViewModel() {
             getNextWord()
         else {
             _currentScrambledWord = String(tempWord)
-            ++currentWordCount
+            ++_currentWordCount
             wordList.add(currentWord)
         }
 
     }
 
     fun nextWord():Boolean{
-        return if(currentWordCount< MAX_NO_OF_WORDS){
+        return if(_currentWordCount< MAX_NO_OF_WORDS){
             getNextWord()
             true
         }else false
@@ -61,7 +64,15 @@ class GameViewModel: ViewModel() {
         }
         return false
     }
-
+    /*
+    Reintializes the game data to restart the game
+     */
+    fun reinitializeData(){
+        _score=0
+        _currentWordCount=0
+        wordList.clear()
+        getNextWord()
+    }
 
 }
 
