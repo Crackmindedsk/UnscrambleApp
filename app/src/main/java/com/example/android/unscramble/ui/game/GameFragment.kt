dfcfacbea6a4e7154b3baac36e12d8a2152618c1
Crackmindedsk <shakumbhar@gmail.com>
2021-06-24 12:33:08 +0530

    Improved

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -74,6 +75,9 @@ class GameFragment : Fragment() {
     private fun onSubmitWord() {
 //
         val playerWord=binding.textInputEditText.text.toString()
+        if(playerWord.isEmpty())
+            Toast.makeText(context, "Enter word", Toast.LENGTH_SHORT).show()
+
 
         if (viewModel.isUserWordCorrect(playerWord)){
             setErrorTextField(false)
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,72 +4,74 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GameViewModel: ViewModel() {
+class GameViewModel : ViewModel() {
 
-    private val _currentWordCount=MutableLiveData(0)
-    val currentWordCount:LiveData<Int>
-    get() = _currentWordCount
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
 
-    private val _currentScrambledWord= MutableLiveData<String>()
+    private val _currentScrambledWord = MutableLiveData<String>()
     val currentScrambledWord: LiveData<String>
-    get() = _currentScrambledWord
+        get() = _currentScrambledWord
 
-    private var _count=0
-    val count:Int
-    get() = _count
+    private var _count = 0
+    val count: Int
+        get() = _count
 
-    private val _score=MutableLiveData(0)
-    val score:LiveData<Int>
-    get() = _score
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
+        get() = _score
 
-    private var wordList:MutableList<String> = mutableListOf()
-    private lateinit var currentWord:String
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     init {
         getNextWord()
     }
 
-    private fun getNextWord(){
-        currentWord= allWordsList.random()
-        val tempWord=currentWord.toCharArray()
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
 
-        while (tempWord.toString().equals(currentWord,false)){
+        while (tempWord.toString().equals(currentWord, false)) {
             tempWord.shuffle()
         }
         if (wordList.contains(currentWord))
             getNextWord()
         else {
             _currentScrambledWord.value = String(tempWord)
-            _currentWordCount.value=_currentWordCount.value?.inc()
+            _currentWordCount.value = _currentWordCount.value?.inc()
             wordList.add(currentWord)
         }
 
     }
 
-    fun nextWord():Boolean{
-        return if(_currentWordCount.value!!< MAX_NO_OF_WORDS){
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
             true
-        }else false
+        } else false
     }
 
-    private fun increaseScore(){
-        _score.value=_score.value?.plus(SCORE_INCREASE)
+    private fun increaseScore() {
+        _score.value = _score.value?.plus(SCORE_INCREASE)
     }
 
-    fun isUserWordCorrect(playerWord:String):Boolean{
-        if (playerWord.equals(currentWord,true)){
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
             increaseScore()
             return true
         }
         return false
     }
+
     /*
     Reintializes the game data to restart the game
      */
-    fun reinitializeData(){
-        _score.value=0
-        _currentWordCount.value=0
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
         wordList.clear()
         getNextWord()
     }
